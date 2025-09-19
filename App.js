67ec9fc80bbd7e285a24d3d6e74e81f3c9c28264
Jonathan <6176721@collegemv.qc.ca>
2025-09-19 08:25:14 -0400

    modif fonctionnalité taille police

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { ThemeProvider } from './src/context/ThemeContext';
+import { FontSizeProvider } from './src/context/FontSizeContext';  
 import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
-
     return (
         <ThemeProvider>
-            <AppNavigator />
+            <FontSizeProvider>
+                <AppNavigator />
+            </FontSizeProvider>
         </ThemeProvider>
     );
 }
