98ebe6fe8dbb5a55a9ca12214e9e98548086064e
Jonathan <6176721@collegemv.qc.ca>
2025-12-19 23:01:26 -0500

    ajout capital appnav

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -5,6 +5,7 @@ import { Animated } from 'react-native';
 import CountrySearchResults from "../components/CountrySearchResults";
 import CountryDetails from '../screens/CountryDetails';
 import SearchScreen from '../screens/CountrySearchScreen';
+import Capital from '../screens/Capital';
 
 const Stack = createNativeStackNavigator();
 
@@ -17,6 +18,7 @@ export default function AppNavigator() {
                 <Stack.Screen name="CountrySearch" children={() => <SearchScreen parallax={parallax} />} />
                 <Stack.Screen name="CountryResults" component={CountrySearchResults}/>
                 <Stack.Screen name="CountryDetails" component={CountryDetails} options={{ headerShown: true, title: "Pays" }} />
+                <Stack.Screen name="Capital" component={Capital} options={{ headerShown: true, title: "Capitales" }} />
             </Stack.Navigator>
         </NavigationContainer>
     );
