d595627272e2826c7bdb199458be980b8e8b86a1
Jonathan <6176721@collegemv.qc.ca>
2025-12-20 11:28:56 -0500

    temp x capital header

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -18,7 +18,7 @@ export default function AppNavigator() {
                 <Stack.Screen name="CountrySearch" children={() => <SearchScreen parallax={parallax} />} />
                 <Stack.Screen name="CountryResults" component={CountrySearchResults}/>
                 <Stack.Screen name="CountryDetails" component={CountryDetails} options={{ headerShown: true, title: "Pays" }} />
-                <Stack.Screen name="CapitalsWeather" component={CapitalsWeather} options={{ title: "Météo des capitales" }}/>
+                <Stack.Screen name="CapitalsWeather" component={CapitalsWeather} options={{ headerShown: true, title: "Météo des capitales" }}/>
             </Stack.Navigator>
         </NavigationContainer>
     );
